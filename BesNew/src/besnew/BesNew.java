15478564462dfe2ef93d047e58c2e17d0558aabf
ChalewK <41241827+ChalewK@users.noreply.github.com>
2019-03-29 23:38:19 -0700

    New trail for bese670

--- BesNew/src/besnew/BesNew.java
+++ BesNew/src/besnew/BesNew.java
@@ -16,6 +16,11 @@ public class BesNew {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    
+        System.out.println("besnew.BesNew.main()");
+    
     }
+    
+    
     
 }
